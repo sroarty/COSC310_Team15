a419f5eafe4d21aabc6722e6481ba3708fbbe79d
Sean Roarty <43792819+sroarty@users.noreply.github.com>
2021-03-11 15:40:40 -0800

    Just testing a couple of things.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+
 'use strict';
-
-const {dialogflow} = require('actions-on-google');
+ 
 const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+const agent = new WebhookClient({ request, response });
+console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
 
-const app = dialogflow({debug: true});
+  function WebCallIntent(agent) {
+    var myParam = agent.parameters["paintypes"];
+	  agent.add(`You passed me ${myParam}`);
+    agent.intent = "Introduction";
+  }
 
-app.intent('Default Welcome Intent', (conv) => {
-  // Do things
+  // // Uncomment and edit to make your own Google Assistant intent handler
+  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  function googleAssistantHandler(agent) {
+    let conv = agent.conv(); // Get Actions on Google library conv instance
+    conv.ask('Hello from the Actions on Google client library!'); // Use Actions on Google library
+    agent.add(conv); // Add Actions on Google library responses to your agent's response
+  }
+  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
+  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('WebCallIntent', WebCallIntent);
+  //intentMap.set('Introduction', yourFunctionHandler);
+  //intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
 });
-exports.yourAction = functions.https.onRequest(app);

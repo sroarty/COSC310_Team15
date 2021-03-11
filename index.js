67030af2a15012dc6f6cbeb70b2d45a552a08f21
ameliaco <77419587+ameliaco@users.noreply.github.com>
2021-03-11 12:11:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,24 @@ const functions = require('firebase-functions');
 
 const app = dialogflow({debug: true});
 
+const QUERYHANDLER_INTENT = 'QueryHandler';
+const LEG_PAIN_INTENT = 'Leg pain';
+const UPPER_LEG_INTENT = 'Upper Leg pain';
+
+const GEN_LOCATION_ENTITY = 'general-location';
+const SPEC_LOCATION_ENTITY = 'specific-location';
+
 app.intent('Default Welcome Intent', (conv) => {
   // Do things
+});
+app.intent(QUERYHANDLER_INTENT, (conv) => {
+  const genpainloc = conv.parameters(GEN_LOCATION_ENTITY);
+  if(genpainloc == "legs"){
+    const specpainloc = conv.parameters(SPEC_LOCATION_ENTITY);
+    conv.ask("Is the pain located in your upper or lower legs?");
+  }
+  else{
+    conv.ask("Where is the pain located?");
+  }
 });
 exports.yourAction = functions.https.onRequest(app);
